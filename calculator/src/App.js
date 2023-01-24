db40dca34792df541bda79833ae7ba0a6cea726d
Nhoj <nhojplummer@yahoo.com>
2023-01-24 17:03:59 -0500

    IMporting components to app and testing for button action

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,12 +1,24 @@
 import React from "react"
 import Wrapper from "./components/Wrapper/Wrapper"
 import Screen from "./components/Screen/Screen"
+import ButtonBox from "./components/ButtonBox/ButtonBox"
+import Button from "./components/Button/Button"
 
 function App() {
   return (
     <>
-      <Wrapper />
-      <Screen />
+      <Wrapper>
+        <Screen value="0" />
+        <ButtonBox>
+          <Button
+            className=""
+            value="0"
+            onClick={() => {
+              console.log("Button Clicked!")
+            }}
+          />
+        </ButtonBox>
+      </Wrapper>
     </>
   )
 }
