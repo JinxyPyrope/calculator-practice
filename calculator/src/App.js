29bd26f083ef2b0fc2486af3a2ea5929c59e60e1
Nhoj <nhojplummer@yahoo.com>
2023-01-24 18:04:56 -0500

    Begin adding the useState piece to the code now

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import Wrapper from "./components/Wrapper/Wrapper"
 import Screen from "./components/Screen/Screen"
 import ButtonBox from "./components/ButtonBox/ButtonBox"
@@ -16,6 +16,12 @@ const btnValues = [
 //The "flat()" method makes a new array wit hall the subarrays combined into whichever level we want to. In this case since we didn't specify the number of "flat" we want it'll drop all arrays down one level
 //The "map()" method makes it so we can create a new array based on wah we can to occur to the previous numbers. In this case we map it out based on the "btn" and "i"
 const App = () => {
+  //Hwew we etup the "useState" to make the buttons work
+  let [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0
+  })
   return (
     <Wrapper>
       <Screen value="0" />
--- calculator/src/components/Button/Button.js
+++ calculator/src/components/Button/Button.js
@@ -1,5 +1,6 @@
 import "./Button.css"
 
+//We connected the buttons to the btnValues in the App.js so that each 
 const Button = ({ className, value, onClick }) => {
   return (
     <button className={className} onClick={onClick}>
