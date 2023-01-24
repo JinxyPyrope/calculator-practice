c59916d132d1ca66eaad9924c1c445de3a2dfcb4
Nhoj <nhojplummer@yahoo.com>
2023-01-24 16:05:08 -0500

    Textfit package installed plus Screen component is created

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Wrapper from "./components/Wrapper/Wrapper"
+import Screen from "./components/Screen/Screen"
 
 function App() {
   return (
     <React.Fragment>
       <Wrapper />
+      <Screen />
     </React.Fragment>
   )
 }
